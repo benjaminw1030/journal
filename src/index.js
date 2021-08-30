358be8d08f259fe50f320fda579498daa266f35a
Messquerade <annac.klingberg@gmail.com>
2021-08-30 16:39:17 -0700

    add css styling

--- src/index.js
+++ src/index.js
@@ -4,23 +4,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-  $('#journal-entry-form').submit(function(event) {
-    event.preventDefault();
-    const title = $('#title').val();
-    const body = $('#body').val();
-    const date = $('#date').val();
-    const entry = new Entry(title, body, date)
-    const entryDate = entry.date;
-    const entryTitle = entry.title;
-    const entryBody = entry.body;
-    $('#journal').append("<h4>" + entryDate + "</h4>");
-    $('#journal').append("<h4>" + entryTitle + "</h4>");
-    $('#journal').append("<h5>" + entry.getTeaser() + "</h5>");
-    $('#journal').append("<p>" + entryBody + "</p>");
-    $('#journal').append("<p>This entry contains " + entry.wordCount() + " words.</p>");
-    $('#journal').append("<p>This entry contains " + entry.vowelCount() + " vowels.</p>");
-    $('#journal').append("<p>This entry contains " + entry.consonantCount() + " consonants.</p>");
-    
-  });
+$('#journal-entry-form').submit(function (event) {
+  event.preventDefault();
+  const title = $('#title').val();
+  const body = $('#body').val();
+  const date = $('#date').val();
+  const entry = new Entry(title, body, date)
+  const entryDate = entry.date;
+  const entryTitle = entry.title;
+  const entryBody = entry.body;
+  $('#journal').append("<div class='card'><h4>" + entryDate + "</h4><h4>" + entryTitle + "</h4><h5>" + entry.getTeaser() + "</h5><p>" + entryBody + "</p><p>This entry contains " + entry.wordCount() + " words.</p><p>This entry contains " + entry.vowelCount() + " vowels.</p><p>This entry contains " + entry.consonantCount() + " consonants.</p></div>");
 });
